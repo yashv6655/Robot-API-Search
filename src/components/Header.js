75f100f33929f96f62650f5c310957a1089d0fda
Yash Velagapudi <yashv6655@gmail.com>
2020-11-06 11:37:24 -0800

    Testing shouldComponentUpdate hook

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,17 @@
 import React, { Component } from "react";
+import CounterButton from "./CounterButton";
 
 export default class Header extends Component {
+  shouldComponentUpdate(nextProps, nextState) {
+    return false;
+  }
+
   render() {
+    console.log("header");
     return (
       <div>
         <h1 className="f1">RoboFriends</h1>
+        <CounterButton color={"red"} />
       </div>
     );
   }
--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 const SearchBox = ({ searchfield, searchChange }) => {
+  console.log("Search box");
   return (
     <div className="pa2">
       <input
